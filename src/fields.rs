f99c35f84c89df514c20e9957875137862df1dbd
Sean Bowe <ewillbefull@gmail.com>
2019-08-21 10:11:06 -0600

    Implementation of primitive fields

---
+++ src/fields.rs
@@ -0,0 +1,57 @@
+use std::fmt::Debug;
+use std::ops::{Add, Mul, Neg, Sub};
+
+pub trait Field:
+    Sized
+    + Copy
+    + Clone
+    + Send
+    + Sync
+    + 'static
+    + Debug
+    + Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Neg<Output = Self>
+    + PartialEq
+    + Eq
+{
+    /// How many bits of information can be stored reliably?
+    const CAPACITY: usize;
+
+    // p - 1 is divisible by 2^s
+    const S: usize;
+
+    /// Generator of the p - 1 order multiplicative subgroup
+    const GENERATOR: Self;
+
+    /// Generator of the 2^S multiplicative subgroup
+    const ALPHA: Self;
+
+    fn from_u64(v: u64) -> Self;
+    fn invert(&self) -> Option<Self>;
+
+    fn zero() -> Self {
+        Self::from_u64(0)
+    }
+    fn one() -> Self {
+        Self::from_u64(1)
+    }
+    fn square(&self) -> Self {
+        (*self) * (*self)
+    }
+    fn pow(&self, by: u32) -> Self {
+        let mut acc = Self::one();
+        for i in (0..32).rev().map(|b| ((by >> b) & 1) == 1) {
+            acc = acc.square();
+            if i {
+                acc = acc * (*self);
+            }
+        }
+        acc
+    }
+}
+
+mod fp;
+
+pub use fp::*;
---
+++ src/fields/fp.rs
@@ -0,0 +1,58 @@
+use crate::fields::Field;
+use std::ops::{Add, Mul, Neg, Sub};
+
+const MODULUS: u32 = 2113929217;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Fp(u32);
+
+impl Field for Fp {
+    const CAPACITY: usize = 30;
+    const S: usize = 25;
+    const GENERATOR: Self = Fp(5);
+    const ALPHA: Self = Fp(1971140334);
+
+    fn from_u64(v: u64) -> Self {
+        Fp((v % (MODULUS as u64)) as u32)
+    }
+
+    fn invert(&self) -> Option<Self> {
+        if self.0 == 0 {
+            None
+        } else {
+            Some(self.pow(MODULUS - 2))
+        }
+    }
+}
+
+impl Add for Fp {
+    type Output = Fp;
+
+    fn add(self, other: Fp) -> Self::Output {
+        Fp((self.0 + other.0) % MODULUS)
+    }
+}
+
+impl Neg for Fp {
+    type Output = Fp;
+
+    fn neg(self) -> Self::Output {
+        Fp((MODULUS - self.0) % MODULUS)
+    }
+}
+
+impl Sub for Fp {
+    type Output = Fp;
+
+    fn sub(self, other: Fp) -> Self::Output {
+        self + (-other)
+    }
+}
+
+impl Mul for Fp {
+    type Output = Fp;
+
+    fn mul(self, other: Fp) -> Self::Output {
+        Fp((((self.0 as u64) * (other.0 as u64)) % (MODULUS as u64)) as u32)
+    }
+}
