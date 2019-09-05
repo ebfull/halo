9f5c8aee386a25ee6062e387bc88eb2751e0bb81
Jack Grigg <jack@z.cash>
2019-09-04 20:02:53 -0400

    Remove old dummy Rescue code

--- src/gadgets/rescue.rs
+++ src/gadgets/rescue.rs
@@ -1,12 +1,7 @@
 use crate::{
-    circuits::{Coeff, ConstraintSystem, IntoLinearCombination, LinearCombination, SynthesisError},
-    curves::Curve,
+    circuits::{Coeff, ConstraintSystem, IntoLinearCombination, SynthesisError},
     fields::Field,
-    gadgets::{
-        boolean::{unpack_fe, AllocatedBit},
-        ecc::CurvePoint,
-        num::{AllocatedNum, Combination, Num},
-    },
+    gadgets::num::{AllocatedNum, Combination, Num},
     rescue::{generate_mds_matrix, RESCUE_M, RESCUE_ROUNDS, SPONGE_RATE},
 };
 use std::ops::AddAssign;
@@ -309,57 +304,6 @@ impl<F: Field> RescueGadget<F> {
         }
     }
 }
-
-/*
-pub fn rescue_gadget<F: Field, CS: ConstraintSystem<F>>(
-    cs: &mut CS,
-    inputs: &[AllocatedNum<F>],
-) -> Result<AllocatedNum<F>, SynthesisError> {
-    let mut cur = Combination::from(Num::constant(F::ALPHA));
-    for input in inputs {
-        for _ in 0..30 {
-            cur = cur + *input;
-            cur = Combination::from(cur.square(cs)?);
-        }
-    }
-
-    cur.square(cs)
-}
-*/
-pub fn rescue<F: Field>(inputs: &[F]) -> F {
-    let mut cur = F::ALPHA;
-    for input in inputs {
-        for _ in 0..30 {
-            cur = cur + *input;
-            cur = cur.square()
-        }
-    }
-
-    cur.square()
-}
-/*
-pub fn append_point<C: Curve, CS: ConstraintSystem<C::Base>>(
-    cs: &mut CS,
-    transcript: &AllocatedNum<C::Base>,
-    point: &CurvePoint<C>,
-) -> Result<AllocatedNum<C::Base>, SynthesisError> {
-    rescue_gadget(cs, &[transcript.clone(), point.x(), point.y()])
-}
-
-pub fn obtain_challenge<F: Field, CS: ConstraintSystem<F>>(
-    cs: &mut CS,
-    transcript: &AllocatedNum<F>,
-) -> Result<(AllocatedNum<F>, Vec<AllocatedBit>), SynthesisError> {
-    let new_transcript = rescue_gadget(cs, &[transcript.clone()])?;
-
-    // We don't need to enforce that it's canonical; the adversary
-    // only gains like a bit out of this.
-    let mut bits = unpack_fe(cs, &transcript)?;
-    bits.truncate(128); // only need lower 128 bits
-
-    Ok((new_transcript, bits))
-}
-*/
 
 #[cfg(test)]
 mod test {
